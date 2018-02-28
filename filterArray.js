8600139bcc4225eecef98c31b5add3f2dc5cb469
Linda Foo <linda.foo.dev@gmail.com>
2018-02-28 11:21:11 -0800

    added drill notes and set-up maxSum drill

--- filterArray.js
+++ filterArray.js
@@ -13,3 +13,8 @@ function filterArray(array) {
 //we want function to take an array of numbers and filter out any numbers less than 5 w/o using .filter method
 
 console.log(filterArray([3, 2, 1, 20, 100, 1000, 0, -3.5]));
+
+/*
+Filtering an array
+Imagine you have an array of numbers. Write an algorithm to remove all numbers less than five from the array. Don't use array's built-in .filter method here; write the algorithm from scratch.
+*/
